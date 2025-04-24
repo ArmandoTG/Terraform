bbb9b4b31965c085c4f20903419bfc0fc9bb4ee5
ArmandoTG <armandotg1108@gmail.com>
2025-04-24 01:00:02 -0500

    Update webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -1,15 +1,18 @@
- terraform {
-  required_version = ">= 0.11" 
- backend "azurerm" {
-  storage_account_name = "__terraformstorageaccount__"
+terraform {
+  required_version = ">= 1.3.0"
+
+  backend "azurerm" {
+    storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
-	access_key  ="__storagekey__"
-	}
-	}
-  provider "azurerm" {
+    access_key           = "__storagekey__"
+  }
+}
+
+provider "azurerm" {
   features {}
 }
+
 resource "azurerm_resource_group" "dev" {
   name     = "PULTerraform"
   location = "West Europe"
@@ -17,8 +20,8 @@ resource "azurerm_resource_group" "dev" {
 
 resource "azurerm_app_service_plan" "dev" {
   name                = "__appserviceplan__"
-  location            = "${azurerm_resource_group.dev.location}"
-  resource_group_name = "${azurerm_resource_group.dev.name}"
+  location            = azurerm_resource_group.dev.location
+  resource_group_name = azurerm_resource_group.dev.name
 
   sku {
     tier = "Free"
@@ -28,8 +31,7 @@ resource "azurerm_app_service_plan" "dev" {
 
 resource "azurerm_app_service" "dev" {
   name                = "__appservicename__"
-  location            = "${azurerm_resource_group.dev.location}"
-  resource_group_name = "${azurerm_resource_group.dev.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
-
+  location            = azurerm_resource_group.dev.location
+  resource_group_name = azurerm_resource_group.dev.name
+  app_service_plan_id = azurerm_app_service_plan.dev.id
 }
